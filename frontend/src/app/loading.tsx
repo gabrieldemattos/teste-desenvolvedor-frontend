ac98fbedde13adc9122197495f3e61a57f806512
Gabriel de Mattos <mattos_95@hotmail.com>
2024-04-17 23:49:50 -0300

    feat: Create loading component

---
+++ frontend/src/app/loading.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="loading-container">
+      <div className="loading-wrapper">
+        <div className="spiner"></div>
+        <p>Carregando dados..</p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
